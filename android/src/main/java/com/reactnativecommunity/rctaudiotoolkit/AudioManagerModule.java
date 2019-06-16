bbd448dfd4aa4ec4a3795f83f87e0ab3bd15bf02
Scott Beca <scott.beca@gmail.com>
2019-06-16 13:08:04 +1000

    More AudioManager work

--- android/src/main/java/com/reactnativecommunity/rctaudiotoolkit/AudioManagerModule.java
+++ android/src/main/java/com/reactnativecommunity/rctaudiotoolkit/AudioManagerModule.java
@@ -30,11 +30,14 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Objects;
 
-public class AudioManagerModule extends ReactContextBaseJavaModule {
+public class AudioManagerModule extends ReactContextBaseJavaModule implements AudioManager.OnAudioFocusChangeListener {
     private static final String LOG_TAG = "AudioManagerModule";
 
     private ReactApplicationContext context;
     private AudioPriority currentAudioPriority = AudioPriority.MIX_WITH_OTHER_APPS;
+    private AudioManager audioManager = null;
+    @TargetApi(21)
+    private AudioFocusRequest previousFocusRequest = null;
 
     // NOTE: Values must be kept in sync with AudioManager.js
     public enum AudioPriority {
@@ -44,6 +47,7 @@ public class AudioManagerModule extends ReactContextBaseJavaModule {
     public AudioManagerModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.context = reactContext;
+        this.audioManager = (AudioManager) Context.getSystemService(Context.AUDIO_SERVICE);
     }
 
     @Override
@@ -58,34 +62,114 @@ public class AudioManagerModule extends ReactContextBaseJavaModule {
             return;
         }
 
-        if (priority == AudioPriority.SILENCE_OTHER_APPS) {
-            if (android.os.Build.VERSION.SDK_INT >= 21) {
-                requestAudioFocus21();
-            } else {
-                requestAudioFocus();
-            }
-        } else {
-
+        if (this.audioManager == null) {
+            this.audioManager = (AudioManager) Context.getSystemService(Context.AUDIO_SERVICE);
         }
 
-        currentAudioPriority = priority;
-        callback.invoke(true);
+        if (priority == AudioPriority.SILENCE_OTHER_APPS) {
+            if (android.os.Build.VERSION.SDK_INT >= 21) {
+                requestAudioFocus21(callback);
+            } else {
+                requestAudioFocus(callback);
+            }
+        } else {
+            if (android.os.Build.VERSION.SDK_INT >= 26) {
+                abandonAudioFocus26(callback);
+            } else {
+                abandonAudioFocus(callback);
+            }
+        }
     }
 
-    private void requestAudioFocus() {
+    private void requestAudioFocus(Callback callback) {
+        int res = this.audioManager.requestAudioFocus(this, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+
+        if (res == AudioManager.AUDIOFOCUS_REQUEST_FAILED) {
+            callback.invoke(false);
+        } else if (res == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            currentAudioPriority = SILENCE_OTHER_APPS;
+            callback.invoke(true);
+        } else {
+            // TODO: Throw unhandled error
+            callback.invoke(false);
+        }
     }
 
     @TargetApi(21)
-    private void requestAudioFocus21() {
-        AudioManager mAudioManager = (AudioManager) Context.getSystemService(Context.AUDIO_SERVICE);
-
-        AudioAttributes mPlaybackAttributes = new AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_MEDIA)
+    private void requestAudioFocus21(Callback callback) {
+        AudioAttributes playbackAttributes = new AudioAttributes.Builder().setUsage(AudioAttributes.USAGE_MEDIA)
                 .setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN).build();
 
-        AudioFocusRequest mFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
-                .setAudioAttributes(mPlaybackAttributes).setAcceptsDelayedFocusGain(true).setWillPauseWhenDucked(false)
+        this.previousFocusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+                .setAudioAttributes(playbackAttributes).setWillPauseWhenDucked(false)
                 .setOnAudioFocusChangeListener(this, mMyHandler).build();
 
-        int res = mAudioManager.requestAudioFocus(mFocusRequest);
+        int res = this.audioManager.requestAudioFocus(this.previousFocusRequest);
+
+        if (res == AudioManager.AUDIOFOCUS_REQUEST_FAILED) {
+            callback.invoke(false);
+        } else if (res == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            currentAudioPriority = SILENCE_OTHER_APPS;
+            callback.invoke(true);
+        } else {
+            // TODO: Throw unhandled error
+            callback.invoke(false);
+        }
+    }
+
+    private void abandonAudioFocus(Callback callback) {
+        int res = this.audioManager.abandonAudioFocusRequest(this);
+
+        if (res == AudioManager.AUDIOFOCUS_REQUEST_FAILED) {
+            callback.invoke(false);
+        } else if (res == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            currentAudioPriority = MIX_WITH_OTHER_APPS;
+            callback.invoke(true);
+        } else {
+            // TODO: Throw unhandled error
+            callback.invoke(false);
+        }
+
+        if (android.os.Build.VERSION.SDK_INT >= 21) {
+            this.previousFocusRequest = null;
+        }
+    }
+
+    @TargetApi(26)
+    private void abandonAudioFocus26(Callback callback) {
+        if (this.previousFocusRequest == null) {
+            // TODO: Throw error
+            callback.invoke(false);
+            return;
+        }
+
+        int res = this.audioManager.abandonAudioFocusRequest(this.previousFocusRequest);
+
+        if (res == AudioManager.AUDIOFOCUS_REQUEST_FAILED) {
+            callback.invoke(false);
+        } else if (res == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            currentAudioPriority = MIX_WITH_OTHER_APPS;
+            callback.invoke(true);
+        } else {
+            // TODO: Throw unhandled error
+            callback.invoke(false);
+        }
+
+        this.previousFocusRequest = null;
+    }
+
+    public void onAudioFocusChange(int focusChange) {
+        // Don't need to do anything here yet
+
+        switch (focusChange) {
+        case AudioManager.AUDIOFOCUS_GAIN:
+        case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT:
+        case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_EXCLUSIVE:
+        case AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK:
+        case AudioManager.AUDIOFOCUS_LOSS:
+        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+        case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+            break;
+        }
     }
 }
